4dda2e83718a09f86b549b4064701e1d8a89a5ce
Jefri Draup <jefri.draup@edfenergy.com>
2022-04-25 22:45:09 +0100

    ready to run

--- articles.py
+++ articles.py
@@ -1,4 +1,4 @@
-adverbs={
+articles={
 "a":"",
 "all":"",
 "an":"",
--- webscrape.py
+++ webscrape.py
@@ -4,7 +4,7 @@ from googlesearch import search
 query = "google translate"
 
 for j in search(query, tld="co.in", num=10, stop=10,pause=2):
-	print(j)
+    print(j)
 '''
 '''
 import requests
@@ -43,95 +43,129 @@ translator.detect(text)
 import time
 import requests
 import html
+import random
 from lxml import html as parser
-list_to_translate=[
-"to add",
-"to agree",
-"to allow",
-"to appear",
-"to ask",
-"to be",
-"to become",
-"to begin",
-"to believe",
-"to bring",
-"to build",
-"to buy",
-"to call",
-"to carry",
-"to change",
-"to come",
-"to consider",
-"to continue",
-"to create",
-"to decide",
-"to describe",
-"to develop",
-"to die",
-"to do",
-"to draw",
-"to except",
-"to fall",
-"to feel",
-"to find",
-"to follow",
-"to get",
-"to give",
-"to go",
-"to happen",
-"to have",
-]
 
-headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0'}
-for i in range(0,len(list_to_translate)):
-	columns=list_to_translate[i].split()
-	search_me=""
-	for j in range(0,len(columns)):
-		if j == len(columns)-1:
-			search_me+=columns[j]
-		else:
-			search_me+=columns[j]+'+'
-	print(search_me,type(search_me))
-	URL="https://www.google.com/search?q=%22"+search_me+"%22+translate+to+persian&client=ubuntu&channel=fs&ei=9PliYrXYCYepgAbHiqroAw&ved=0ahUKEwi1oveTrKj3AhWHFMAKHUeFCj0Q4dUDCA4&uact=5&oq=%22i+am+here%22+translate+to+persian&gs_lcp=Cgdnd3Mtd2l6EAM6BwgAEEcQsAM6BQgAEKIEOgUIABCRAjoKCAAQsQMQgwEQQzoLCAAQgAQQsQMQgwE6DgguEIAEELEDEMcBEKMCOg4ILhCABBCxAxDHARDRAzoLCC4QgAQQsQMQgwE6CAgAEIAEELEDOgoILhCxAxDUAhBDOgQILhBDOgcILhCxAxBDOgsILhCABBDHARCvAToOCC4QgAQQsQMQxwEQrwE6CAguEIAEENQCOgUIABCABDoKCAAQkQIQRhD_AToECAAQAzoFCC4QgAQ6CggAEIAEEEYQ_wE6BggAEBYQHjoICCEQFhAdEB46BwghEAoQoAFKBAhBGABKBAhGGABQ9QtYtYMBYM-FAWgHcAF4AYABnAWIAdU3kgEMNC4yOC4zLjEuMC4ymAEAoAEByAEIwAEB&sclient=gws-wiz"
-	image_URL="https://www.google.com/search?q="+search_me+"&client=ubuntu&hs=Nxt&channel=fs&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj4mriK3Kn3AhVNeMAKHccDDIYQ_AUoAXoECAEQAw&biw=1920&bih=944&dpr=1#imgrc=u3Q2KHasDPX2BM"
-	page = requests.get(URL,headers=headers)
-	image_page = requests.get(image_URL,headers=headers)
-	tree = parser.fromstring(page.content)
-	image_tree = parser.fromstring(image_page.content)
-	translation = tree.xpath('//pre[@id="tw-target-text"]/span[@class="Y2IQFc"]/text()')
-	print(translation)
-	#image_dl_link=image_tree.xpath('//div[@class="mJxzWe"]')
-	#image_dl_link=image_tree.xpath('//div[@class="isv-r PNCib MSM1fd BUooTd"]')
-	#image_dl_link=image_tree.xpath('//img[@class="rg_i Q4LuWd"]')
-	image_dl_link=image_tree.xpath('//a[@class="wXeWr islib nfEiy"]')
-	image_dl_link=image_tree.xpath('//a[@href]')
-	print(image_dl_link[0].values()[0])
-	f=open("search","w")
-	f.write(image_page.text)
-	f.close()
-	print(dir(image_dl_link[0]))
-	print(image_dl_link[0])
-	print(image_dl_link[0].keys())
-	print(image_dl_link[0].values())
-	#print(image_dl_link[0].resolve_base_ref)
-	#print(image_dl_link[0].get("data-iid"))
-	if i == 0:
-		f=open("translations.py","w")
-		f.write("verbs={\n")
-	else:
-		f=open("translations.py","a")
-	
-	write_me = '\"' + list_to_translate[i] + '\"' + ' : \"' +str(translation).strip("'[]") + '\",\n'
-	f.write(write_me)
-	
+from verbs import verbs
+from adverbs import adverbs
+from articles import articles
+from conjunctions import conjunctions
+from interjections import interjections
+from modal_verb import modal_verbs
+from nouns import nouns
+from prepositions import prepositions
+from pronouns import pronouns
 
-	if i == (len(list_to_translate)-1):
-		f.write("}\n")
-	f.close()
+list_of_dicts = ["verbs", "adverbs", "articles", "conjunctions", "interjections", "modal_verbs", "nouns", "prepositions", "pronouns"]
+list_of_words = [verbs, adverbs, articles, conjunctions, interjections, modal_verbs, nouns, prepositions, pronouns]
+lists_2_trans=[]
+for i in range(0,len(list_of_words)):
+    lists_2_trans.append(list(list_of_words[i].keys()))
 
-	time.sleep(0.01)
+# list_to_translate=[
+# "to add",
+# "to agree",
+# "to allow",
+# "to appear",
+# "to ask",
+# "to be",
+# "to become",
+# "to begin",
+# "to believe",
+# "to bring",
+# "to build",
+# "to buy",
+# "to call",
+# "to carry",
+# "to change",
+# "to come",
+# "to consider",
+# "to continue",
+# "to create",
+# "to decide",
+# "to describe",
+# "to develop",
+# "to die",
+# "to do",
+# "to draw",
+# "to except",
+# "to fall",
+# "to feel",
+# "to find",
+# "to follow",
+# "to get",
+# "to give",
+# "to go",
+# "to happen",
+# "to have",
+# ]
+
+
+user_agents=[#'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0',
+'Mozilla/5.0 (X11; Linux i686; rv:99.0) Gecko/20100101 Firefox/99.0',
+'Mozilla/5.0 (Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0',
+'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:99.0) Gecko/20100101 Firefox/99.0',
+'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0',
+'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0',]
+
+
+for iii in range(0,len(lists_2_trans)):
+    list_to_translate=lists_2_trans[iii]
+    print(list_to_translate)
+    for i in range(0,len(list_to_translate)):
+        columns=list_to_translate[i].split()
+        search_me=""
+        for j in range(0,len(columns)):
+            if j == len(columns)-1:
+                search_me+=columns[j]
+            else:
+                search_me+=columns[j]+'+'
+        print(search_me,type(search_me))
+        headers={'User-Agent':random.choice(user_agents)}
+        URL="https://www.google.com/search?q=%22"+search_me+"%22+translate+to+persian&client=ubuntu&channel=fs&ei=9PliYrXYCYepgAbHiqroAw&ved=0ahUKEwi1oveTrKj3AhWHFMAKHUeFCj0Q4dUDCA4&uact=5&oq=%22i+am+here%22+translate+to+persian&gs_lcp=Cgdnd3Mtd2l6EAM6BwgAEEcQsAM6BQgAEKIEOgUIABCRAjoKCAAQsQMQgwEQQzoLCAAQgAQQsQMQgwE6DgguEIAEELEDEMcBEKMCOg4ILhCABBCxAxDHARDRAzoLCC4QgAQQsQMQgwE6CAgAEIAEELEDOgoILhCxAxDUAhBDOgQILhBDOgcILhCxAxBDOgsILhCABBDHARCvAToOCC4QgAQQsQMQxwEQrwE6CAguEIAEENQCOgUIABCABDoKCAAQkQIQRhD_AToECAAQAzoFCC4QgAQ6CggAEIAEEEYQ_wE6BggAEBYQHjoICCEQFhAdEB46BwghEAoQoAFKBAhBGABKBAhGGABQ9QtYtYMBYM-FAWgHcAF4AYABnAWIAdU3kgEMNC4yOC4zLjEuMC4ymAEAoAEByAEIwAEB&sclient=gws-wiz"
+        image_URL="https://www.google.com/search?q="+search_me+"&client=ubuntu&hs=Nxt&channel=fs&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj4mriK3Kn3AhVNeMAKHccDDIYQ_AUoAXoECAEQAw&biw=1920&bih=944&dpr=1#imgrc=u3Q2KHasDPX2BM"
+        page = requests.get(URL,headers=headers)
+        print("URL",URL)
+        ####image_page = requests.get(image_URL,headers=headers)
+        tree = parser.fromstring(page.content)
+        ####image_tree = parser.fromstring(image_page.content)
+        translation = tree.xpath('//pre[@id="tw-target-text"]/span[@class="Y2IQFc"]/text()')
+        print(translation)
+        if i == 10:
+            break
+        #image_dl_link=image_tree.xpath('//div[@class="mJxzWe"]')
+        #image_dl_link=image_tree.xpath('//div[@class="isv-r PNCib MSM1fd BUooTd"]')
+        #image_dl_link=image_tree.xpath('//img[@class="rg_i Q4LuWd"]')
+        ####image_dl_link=image_tree.xpath('//a[@class="wXeWr islib nfEiy"]')
+        ####image_dl_link=image_tree.xpath('//a[@href]')
+        ####print(image_dl_link[0].values()[0])
+        ####f=open("search","w")
+        ####f.write(image_page.text)
+        ####f.close()
+        ####print(dir(image_dl_link[0]))
+        ####print(image_dl_link[0])
+        ####print(image_dl_link[0].keys())
+        ####print(image_dl_link[0].values())
+        #print(image_dl_link[0].resolve_base_ref)
+        #print(image_dl_link[0].get("data-iid"))
+        if i == 0:
+            f=open(list_of_dicts[iii]+"_scraped.py","w")
+            f.write(list_of_dicts[iii]+"={\n")
+        else:
+            f=open(list_of_dicts[iii]+"_scraped.py","a")
+
+        write_me = '\"' + list_to_translate[i] + '\"' + ' : \"' +str(translation).strip("'[]") + '\",\n'
+        f.write(write_me)
+
+
+        if i == (len(list_to_translate)-1):
+            f.write("}\n")
+        f.close()
+
+        time.sleep(10.)
 
 ###
+
 '''
 headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0'}
 URL="https://www.google.com/search?q=%22to+run%22+translate+to+persian&client=ubuntu&channel=fs&ei=9PliYrXYCYepgAbHiqroAw&ved=0ahUKEwi1oveTrKj3AhWHFMAKHUeFCj0Q4dUDCA4&uact=5&oq=%22i+am+here%22+translate+to+persian&gs_lcp=Cgdnd3Mtd2l6EAM6BwgAEEcQsAM6BQgAEKIEOgUIABCRAjoKCAAQsQMQgwEQQzoLCAAQgAQQsQMQgwE6DgguEIAEELEDEMcBEKMCOg4ILhCABBCxAxDHARDRAzoLCC4QgAQQsQMQgwE6CAgAEIAEELEDOgoILhCxAxDUAhBDOgQILhBDOgcILhCxAxBDOgsILhCABBDHARCvAToOCC4QgAQQsQMQxwEQrwE6CAguEIAEENQCOgUIABCABDoKCAAQkQIQRhD_AToECAAQAzoFCC4QgAQ6CggAEIAEEEYQ_wE6BggAEBYQHjoICCEQFhAdEB46BwghEAoQoAFKBAhBGABKBAhGGABQ9QtYtYMBYM-FAWgHcAF4AYABnAWIAdU3kgEMNC4yOC4zLjEuMC4ymAEAoAEByAEIwAEB&sclient=gws-wiz"
